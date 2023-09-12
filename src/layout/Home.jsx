c86595303d39faf0724f4948ebb942df592b3ba6
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-12 20:02:50 +0600

    feat: Navigation pages added

---
+++ src/layout/Home.jsx
@@ -0,0 +1,11 @@
+import Navigation from "../shared/Navigation/Navigation";
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>  
+        </div>
+    );
+};
+
+export default Home;    <h2>Hello</h2>

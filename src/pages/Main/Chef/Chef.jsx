52eb503e10c93552dd071fad076f483f0a780e51
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-14 20:11:35 +0600

    feat: React to pdf added but this not loading bcoz pakeage outdated

--- src/pages/Main/Chef/Chef.jsx
+++ src/pages/Main/Chef/Chef.jsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import LazyLoad from 'react-lazyload';
-
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 

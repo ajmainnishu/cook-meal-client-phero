0008a8ece445d830b52c2d7a76704ffffff14478
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-14 17:58:21 +0600

    feat: Footer added

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../shared/Navigation/Navigation";
+import Footer from "../shared/Footer/Footer";
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
